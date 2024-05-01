3657129d4b638c667f7aeb61bf47e9f69741439e
«irinabassina» <albas01@bk.ru>
2024-05-02 03:08:25 +0600

    added result

---
+++ go.mod
@@ -0,0 +1,3 @@
+module hw4_merging_sorted_arrays
+
+go 1.22.2
---
+++ main.go
@@ -0,0 +1,48 @@
+package main
+
+import (
+	"fmt"
+	"sort"
+)
+
+func mergeSort(a, b []int) (res []int) {
+	lenA, lenB := len(a), len(b)
+	res = make([]int, 0, lenA+lenB)
+
+	idxA, idxB := 0, 0
+	for idxA+idxB < lenA+lenB {
+		if idxB == lenB || (idxA < lenA && a[idxA] < b[idxB]) {
+			res = append(res, a[idxA])
+			idxA++
+		} else {
+			res = append(res, b[idxB])
+			idxB++
+		}
+
+	}
+
+	return res
+}
+
+// func sortArr(a []int, b []int) ([]int, []int) {
+// 	sort.Ints(a[:])
+// 	sort.Ints(b[:])
+
+// 	return a, b
+// }
+
+func sortArr(c []int, d []int) ([]int, []int) {
+	sort.Ints(c[:])
+	sort.Ints(d[:])
+
+	return c, d
+}
+
+func main() {
+
+	array1 := []int{4, 25, 132, 43}
+	array2 := []int{1, 27, 13, 142, 90}
+
+	mergedArray := mergeSort(sortArr(array1, array2))
+	fmt.Println("Merged array is:", mergedArray)
+}
